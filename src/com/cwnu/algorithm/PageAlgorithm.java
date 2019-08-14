78a496a5d1ccf9e6c2e4db49e9e4e18925bde51b
yangyang6 <449542586@qq.com>
2019-08-14 10:22:52 +0800

    readme

---
+++ src/com/cwnu/algorithm/PageAlgorithm.java
@@ -0,0 +1,17 @@
+package com.cwnu.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rey on 2019-08-14.
+ */
+public class PageAlgorithm {
+
+    public static final int PAGE_SIZE = 20;
+
+    public static void main(String[] args) {
+        List<String> ums = new ArrayList<>();
+
+    }
+}
